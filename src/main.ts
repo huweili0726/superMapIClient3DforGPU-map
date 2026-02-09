94bf48ccd060d9fef475f1d64218a1cfbcfcad85
huweili <czxyhuweili@163.com>
2026-02-09 10:32:04 +0800

    pinia 引入

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './router'
+import pinia from './stores'
+import './assets/css/global.css'
+import './assets/css/map.css'
+
 
 const app = createApp(App)
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
