8f09e188938cbe487d7a448a4a9566cf2a786ee0
huweili <czxyhuweili@163.com>
2026-02-09 11:04:18 +0800

    地图瓦片从配置文件中加载

--- src/components/supermap/index.vue
+++ src/components/supermap/index.vue
@@ -64,21 +64,33 @@ const initSuperMap3D = async () => {
   map.scenePromise.then(() => {
     //初始化场景（由于WebGPU采用异步加载，初始化场景需要放在回调中打开）	
 
-    // 将高德瓦片作为底图添加到场景
-    map.imageryLayers.addImageryProvider(new SuperMap3D.UrlTemplateImageryProvider({
-      // 高德瓦片地址模板（{s} 用于多域名负载均衡）
-      url: 'https://webst0{s}.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
-      // 配置子域名（对应 {s}）
-      subdomains: ['1', '2', '3', '4'],
-      // 瓦片投影坐标系（高德默认是墨卡托，对应 EPSG:3857）
-      projection: SuperMap3D.WebMercatorProjection,
-      // 瓦片层级范围
-      minimumLevel: 0,
-      maximumLevel: 18,
-      // 瓦片大小 （默认256，行业标准 ：几乎所有主流地图服务（包括高德地图）都使用 256x256 的瓦片大小）
-      tileWidth: 256,
-      tileHeight: 256,
-    }));
+    // 从配置中加载底图
+    if (mapOptions && mapOptions.basemaps) {
+      // 查找show为true的底图
+      const activeBasemap = mapOptions.basemaps.find((basemap: any) => basemap.show === true)
+      
+      if (activeBasemap) {
+        // 除默认底图 (不执行这行代码，高德地图会被 Cesium 默认底图覆盖，导致你看不到高德地图)
+        map.imageryLayers.removeAll()
+
+        // 加载配置的底图
+        map.imageryLayers.addImageryProvider(new SuperMap3D.UrlTemplateImageryProvider({
+          // 高德瓦片地址模板（{s} 用于多域名负载均衡）
+          url: activeBasemap.url,
+          // 配置子域名（对应 {s}）
+          subdomains: ['1', '2', '3', '4'], // 通过多子域名分散请求，突破浏览器并发限制，让地图加载更快、更稳定。
+          // 瓦片投影坐标系（高德默认是墨卡托，对应 EPSG:3857）
+          projection: SuperMap3D.WebMercatorProjection,
+          // 瓦片层级范围
+          minimumLevel: 0,
+          maximumLevel: 18,
+          // 瓦片大小 （默认256，行业标准 ：几乎所有主流地图服务（包括高德地图）都使用 256x256 的瓦片大小）
+          tileWidth: 256,
+          tileHeight: 256,
+          credit: activeBasemap.name // 用于配置版权 / 来源声明的参数 (显示底图名称)
+        })); 
+      }
+    }
 
     checkWebGPUStatus(map); // 检测 WebGPU 状态
 
@@ -94,16 +106,18 @@ const initSuperMap3D = async () => {
       duration: mapOptions.scene.center.duration // 飞行时间（秒）
     });
 
-    map.scene.sun.show = true; // 显示太阳
+    map.scene.sun.show = false; // 显示太阳
     map.scene.sun.intensity = 1.0; // 太阳强度
     map.scene.moon.show = true; // 显示月球
     map.scene.moon.intensity = 1.0; // 月球强度
+    // 显示地面大气效果
+    map.scene.globe.showGroundAtmosphere = mapOptions.scene.globe.showGroundAtmosphere;
     // 开启/关闭 地球光照效果
     map.scene.globe.enableLighting = mapOptions.scene.globe.enableLighting
     // 显示帧速（FPS）
     map.scene.debugShowFramesPerSecond = true;
     // 开启地形深度测试，确保在地形上的实体正确渲染
-    map.scene.globe.depthTestAgainstTerrain = true;
+    map.scene.globe.depthTestAgainstTerrain = mapOptions.scene.globe.depthTestAgainstTerrain;
   });
 }
 
