e6a047711ef528f223305d55bc1e7debaaf6afc4
huweili <czxyhuweili@163.com>
2026-02-09 08:48:10 +0800

    地图插件放public 里面

---
+++ public/supermap/web/locales/en-US/resources.js
@@ -0,0 +1,145 @@
+window.webResources = {
+    "app": "SuperMap iClient 11i(2022)",
+    "nav": {
+        "home": {
+            "_name": "Home"
+        },
+        "overview": {
+            "_name": "overview",
+            "APIChange": "API Change",
+            "newAPI": "New Class Interface Description",
+        },
+        "product": {
+            "_name": "Product",
+            "introduce": "Product Introduction",
+            "download": "Download",
+            "guide": "Development Guide",
+            "modules": "Module Description",
+            "license": "License Description",
+            "environment": "Environment Configuration",
+        },
+        "webgl": {
+            "_name": "WebGL/WebGPU"
+        },
+        "cesium": {
+            "_name": "Cesium"
+        },
+        "guide": {
+            "_name": "Development Guide",
+            "beginning": "Introduction to Development",
+            "update": "iClient 8C Upgrade",
+            "ie8": "Compatible with IE8",
+            "ready": "Prepare",
+            "multiProjection": "Add Layer",
+            "createMap": "Create a 3D Scene",
+            "vectorTile": "Use Vector Tiles",
+            "addControl": "Add Control",
+            "SetScene": "Scene Setting",
+            "Query": "Query",
+            "Measure": "Measure",
+            "DrawHandler": "Draw",
+            "Analysis": "3D Spatial Analysis",
+            "drawSymbolsAndGraphs": "Draw symbols and graphics",
+            "areaAndDistanceMeasurement": "Area and Distance Measurement",
+            "queryMapInformation": "Feature Query",
+            "editMapInformation": "Flat Edit",
+            "thematicMap": "Thematic Map",
+            "spatialAnalyst": "Spatial Analysis",
+            "networkAnalysis": "Network Analysis",
+            "clientComputing": "Client Computing",
+            "addressMatch": "Address Match",
+            "bigDataAnalysis": "Big Data Analysis",
+            "dataVisualization": "Data Visualization",
+            "currentTimeVisualization": "Data Stream",
+            "ic9TrafficTransferAnalystService": "Traffic Transfer Analysis",
+            "leafletDynamicPlot": "Dynamic Plot"
+        },
+        "examples": {
+            "_name": "Example",
+            "examples": "Example Program",
+        },
+        "api": {
+            "_name": "API",
+            "webglDoc": "Document-WebGL 3D",
+        },
+        "apichange": {
+            "_name": "API Change",
+            "newAPI": "New Class Interface Description",
+        },
+        "technicalTopics": {
+            "_name": "Technical Topic"
+        },
+        "topicType": {
+            "dataLoad":"Data Load",
+            "dataProcess":"Data Process",
+            "effectProduction":"Effect Production",
+            "performance":"Performance Optimization",
+            "other":"Other",
+        },
+        "topic": {
+            "topicDOC": "Technical Topic",
+            "WebGPU": "HowToUseWebGPU",
+            "aqom": "Oblique Photogrammetric Model Attributes",
+            "Bofw": "Beatifucation and optimization common setting for 3D scene in WebGL",
+            // "dpt": "Dynamic Plotting Topics",
+            "fmt": "Flight Management Topics",
+            "token": "Token Instruction",
+            "BIM": "Load BIM and Man-made model",
+            "Gltf": "Load Gltf 3D Model",
+            "imagery": "Load Imagery Data",
+            "LOM": "Load Oblique Photogrammetric Model",
+            "terrain": "Load Terrain Data",
+            "webapi": "Local_Deployment_and_Execution_Guide_for_Examples_in_SuperMap_iClient3D_for_WebGLWebGPU_Product_Package",
+            "DWG": "Point plug-in model sub-object",
+            "thematicMap": "Set thematic map according to the field",
+            "UPBR": "Using Json file to modify PBR displaying effect in WebGL",
+            "topVue": "Vue+WebGL development notes",
+            "subdomains": "Subdomain environment construction and use",
+            "3Ds Max": "Display Problem of 3Ds Max Model",
+            "Bentley": "Instruction of Export Coordinate and Model of Plugin for Bentley",
+            "BIM Factory": "Optimization on Performace and Process Method of BIM Factory",
+            "Terrain to TIN": "Process of Large-Scale Terrain to TIN",
+            "Terrain to Imagery": "Process of Large-Scale Terrain to Imagery",
+            "Oblique Photogrammetry": "Process of Large-Scale Oblique Photogrammetry",
+            "Geological Model": "Solutions on Geological Model",
+            "Man-made Model": "Precautions And Process of Man-made Model",
+            "Vector Region Stretching": "Process and Ideas of Vector Region Stretching",
+            "BPOW": "Beatifucation and optimization common setting for 3D scene in WebGL",
+			"PPOCFAE": "Production Process Of Connectivity Flood Analysis Effect",
+			"CFE":"Create Flow Effects for Pipes",
+			"GDP": "Guide to Data Processing and Visualization for Models with a Time Dimension",
+			"HST":"Optimization of 3D Hardware Configuration for SuperMap GIS‌"
+        },
+        "vuecomps": {
+            "_name": "vue Comps",
+            "v2name": "vue2.0 Component",
+            "v2DevGuide": "Vue2 Component Development Guide",
+            "v2Examples": "Vue2 Sample Code",
+            "v3name": "vue3.0 Component",
+            "v3DevGuide": "Vue3 Component Development Guide",
+            "v3Examples": "Vue2 Sample Code",
+          },
+          "earth": {
+            "_name": "iEarth",
+            "iEarth": "SuperMap iEarth",
+          },
+          "envreport": {
+            "_name": "Env Detection",
+            "enviromentReport": "iClient3D-WebGL Report",
+          },
+
+        "more": {
+            "language": {
+                "_name": "Language"
+            },
+            "theme": {
+                "_name": "Theme Change",
+                'light': "Light Theme",
+                'dark': "Dark Theme"
+            },
+            "faq": {
+                "_name": "FAQ"
+            }
+        },
+    },
+}
